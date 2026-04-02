d04e3942ddc58ae67e3116d6afd9ae832fe403b5
Jeroen Vermeulen <jeroen@deovero.com>
2026-04-02 11:37:27 +0200

    Only menu under Plugins

--- netbox_ssh_keys/navigation.py
+++ netbox_ssh_keys/navigation.py
@@ -1,26 +1,20 @@
-from netbox.plugins import PluginMenu, PluginMenuButton, PluginMenuItem
+from netbox.plugins import PluginMenuButton, PluginMenuItem
 
-menu = PluginMenu(
-    label='SSH Keys',
-    groups=(
-        ('SSH Keys', (
-            PluginMenuItem(
-                link='plugins:netbox_ssh_keys:sshkey_list',
-                link_text='SSH Keys',
-                buttons=(
-                    PluginMenuButton(
-                        link='plugins:netbox_ssh_keys:sshkey_add',
-                        title='Add',
-                        icon_class='mdi mdi-plus-thick',
-                    ),
-                    PluginMenuButton(
-                        link='plugins:netbox_ssh_keys:sshkey_bulk_authorized_keys',
-                        title='Import',
-                        icon_class='mdi mdi-upload',
-                    ),
-                ),
+menu_items = (
+    PluginMenuItem(
+        link='plugins:netbox_ssh_keys:sshkey_list',
+        link_text='SSH Keys',
+        buttons=(
+            PluginMenuButton(
+                link='plugins:netbox_ssh_keys:sshkey_add',
+                title='Add',
+                icon_class='mdi mdi-plus-thick',
             ),
-        )),
+            PluginMenuButton(
+                link='plugins:netbox_ssh_keys:sshkey_bulk_authorized_keys',
+                title='Import',
+                icon_class='mdi mdi-upload',
+            ),
+        ),
     ),
-    icon_class='mdi mdi-key-chain',
 )
